d1e9d721e814bac2d6d17c4e0ea0b49bb16d7f44
PakhomovaAnastasiia <pakhomova@sednor.net>
2024-07-13 22:52:51 +0300

    fixed apollo server for prod

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -4,8 +4,9 @@ import { removeTypenameFromVariables } from '@apollo/client/link/remove-typename
 const removeTypenameLink = removeTypenameFromVariables();
 const link = from([
     removeTypenameLink,
-    new HttpLink({ uri: 'http://localhost:3000/api/graphql' }),
-    new HttpLink({ uri: 'https://ph-books-storage.vercel.app/api/graphql' })
+    new HttpLink({
+        uri: `http${process.env.NODE_ENV === 'development' ? '://localhost:3000' : 's://ph-books-storage.vercel.app'}/api/graphql`
+    })
 ]);
 
 export const apolloClient = new ApolloClient({
