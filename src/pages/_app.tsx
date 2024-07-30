c4d992848340387b6270cae89cf1050478dba4ae
PakhomovaAnastasiia <pakhomova@sednor.net>
2024-07-30 14:05:23 +0300

    removed unused props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,8 +13,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <ApolloProvider client={apolloClient}>
             <Head>
                 <title>Books Storage</title>
-                <meta name="viewport"
-                      content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
             </Head>
 
             <AuthProvider>
