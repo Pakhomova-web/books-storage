e08cdb0d664e42e2b728b7eb8289021d8f7ea3c9
PakhomovaAnastasiia <pakhomova@sednor.net>
2024-10-16 23:04:01 +0300

    fixed books pagination for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Box, Grid, Table, TableFooter, TablePagination, TableRow } from '@mui/m
 import React, { useState } from 'react';
 import { useBooks } from '@/lib/graphql/queries/book/hook';
 import Loading from '@/components/loading';
-import { positionRelative, styleVariables } from '@/constants/styles-variables';
+import { pageStyles, positionRelative, styleVariables } from '@/constants/styles-variables';
 import { BookEntity, IBookFilter, IPageable } from '@/lib/data/types';
 import { styled } from '@mui/material/styles';
 import { useRouter } from 'next/router';
@@ -64,36 +64,40 @@ export default function Home() {
         <Box sx={positionRelative}>
             <Loading show={loading}></Loading>
 
-            <Grid container justifyContent="center">
-                {items.map(((book, i) =>
-                        <StyledGrid item key={i} xl={1} lg={2} md={3} sm={4} xs={6} p={2} textAlign="center"
-                                    onClick={() => handleClickOnBook(book)}>
-                            <Box sx={bookBoxStyles} mb={1}>
-                                <img alt="Image" width="100%" height="100%" style={{ objectFit: 'contain' }}
-                                     src={book.imageId ? `https://drive.google.com/thumbnail?id=${book.imageId}&sz=w1000` : '/book-empty.jpg'}/>
-                            </Box>
-                            <Box sx={bookInfoStyles}>
-                                {book.bookSeries.publishingHouse.name}{book.bookSeries.name === '-' ? '' : `, ${book.bookSeries.name}`}
-                            </Box>
-                            <Box sx={bookTitleStyles}>{book.name}</Box>
-                            <Box sx={bookPriceStyles} mt={1}>{renderPrice(book.price)} грн</Box>
-                        </StyledGrid>
-                ))}
-            </Grid>
+            <Box sx={pageStyles}>
+                <Grid container justifyContent="center">
+                    {items.map(((book, i) =>
+                            <StyledGrid item key={i} xl={1} lg={2} md={3} sm={4} xs={6} p={2} textAlign="center"
+                                        onClick={() => handleClickOnBook(book)}>
+                                <Box sx={bookBoxStyles} mb={1}>
+                                    <img alt="Image" width="100%" height="100%" style={{ objectFit: 'contain' }}
+                                         src={book.imageId ? `https://drive.google.com/thumbnail?id=${book.imageId}&sz=w1000` : '/book-empty.jpg'}/>
+                                </Box>
+                                <Box sx={bookInfoStyles}>
+                                    {book.bookSeries.publishingHouse.name}{book.bookSeries.name === '-' ? '' : `, ${book.bookSeries.name}`}
+                                </Box>
+                                <Box sx={bookTitleStyles}>{book.name}</Box>
+                                <Box sx={bookPriceStyles} mt={1}>{renderPrice(book.price)} грн</Box>
+                            </StyledGrid>
+                    ))}
+                </Grid>
 
-            <Table>
-                <TableFooter>
-                    <TableRow>
-                        <TablePagination rowsPerPageOptions={[5, 10, 25]}
-                                         count={totalCount}
-                                         page={pageSettings.page}
-                                         sx={paginatorStyles}
-                                         rowsPerPage={pageSettings.rowsPerPage}
-                                         onPageChange={(_e, val: number) => onPageChange(val)}
-                                         onRowsPerPageChange={({ target }) => onRowsPerPageChange(Number(target.value))}/>
-                    </TableRow>
-                </TableFooter>
-            </Table>
+                <Box sx={{ position: 'sticky', bottom: 0 }}>
+                    <Table>
+                        <TableFooter>
+                            <TableRow>
+                                <TablePagination rowsPerPageOptions={[5, 10, 25]}
+                                                 count={totalCount}
+                                                 page={pageSettings.page}
+                                                 sx={paginatorStyles}
+                                                 rowsPerPage={pageSettings.rowsPerPage}
+                                                 onPageChange={(_e, val: number) => onPageChange(val)}
+                                                 onRowsPerPageChange={({ target }) => onRowsPerPageChange(Number(target.value))}/>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </Box>
+            </Box>
         </Box>
     );
 }
