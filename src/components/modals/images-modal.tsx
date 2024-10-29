6902afa4bfa0dc236e0c3ef2180d53b58c0b558a
PakhomovaAnastasiia <pakhomova@sednor.net>
2024-10-29 20:32:52 +0200

    centered images in images modal

--- src/components/modals/images-modal.tsx
+++ src/components/modals/images-modal.tsx
@@ -17,7 +17,7 @@ interface IImagesModalProps {
 export default function ImagesModal(props: IImagesModalProps) {
     return (
         <CustomModal open={props.open} onClose={props.onClose} big={true}>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} display="flex" justifyContent="center">
                 <Grid item xs={12} md={3} lg={2}>
                     <StyledImageBox>
                         <CustomImage imageId={props.imageIds[0]}></CustomImage>
