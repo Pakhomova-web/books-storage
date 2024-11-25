0ef4a2708a0b028e2b1b052c882c9c8cd91195d6
PakhomovaAnastasiia <pakhomova@sednor.net>
2024-11-25 17:41:08 +0200

    fixed missed age option

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -5,9 +5,10 @@ export const ageOptions: IOption<number>[] = [
     { label: '1 - 2', id: 2 },
     { label: '2 - 3', id: 3 },
     { label: '3 - 4', id: 4 },
-    { label: '5 - 6', id: 5 },
-    { label: '6 - 7', id: 6 },
-    { label: '7 - 8', id: 7 },
-    { label: '8 - 9', id: 8 },
+    { label: '4 - 5', id: 5 },
+    { label: '5 - 6', id: 6 },
+    { label: '6 - 7', id: 7 },
+    { label: '7 - 8', id: 8 },
+    { label: '8 - 9', id: 9 },
     { label: '9 - 10', id: 10 }
 ];
