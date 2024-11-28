16c6a3ea3521c8c46b56f0a13c1bab36e83b0d98
PakhomovaAnastasiia <pakhomova@sednor.net>
2024-11-28 21:58:45 +0200

    changed displaying discount section

--- src/components/books/discount-books.tsx
+++ src/components/books/discount-books.tsx
@@ -1,15 +1,16 @@
-import { Button, Grid } from '@mui/material';
+import { Button, Grid, useTheme } from '@mui/material';
 import { styleVariables } from '@/constants/styles-variables';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import BooksList from './books-list';
 import React from 'react';
 import { useBooksWithDiscount } from '@/lib/graphql/queries/book/hook';
 import { useRouter } from 'next/router';
-
-const rowsPerPageBooksWithDiscount = 3;
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function DiscountBooks() {
-    const { loading, items } = useBooksWithDiscount(rowsPerPageBooksWithDiscount);
+    const theme = useTheme();
+    const mobileMatches = useMediaQuery(theme.breakpoints.down('md'));
+    const { loading, items } = useBooksWithDiscount(mobileMatches ? 2 : 5);
     const router = useRouter();
 
     return (
@@ -18,7 +19,7 @@ export default function DiscountBooks() {
               <Grid item xs={12} sx={styleVariables.sectionTitle}>
                 Акційні товари
 
-                  {items?.length === rowsPerPageBooksWithDiscount &&
+                  {items?.length === (mobileMatches ? 2 : 5) &&
                     <Button variant="outlined" onClick={() => router.push(`/books?withDiscount=true`)}>
                       Дивитися усі<ArrowForwardIcon/></Button>}
               </Grid>
