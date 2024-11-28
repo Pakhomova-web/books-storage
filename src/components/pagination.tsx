5d2095d505641cc41cb91f051c0ba23743ad1e61
PakhomovaAnastasiia <pakhomova@sednor.net>
2024-11-28 16:40:57 +0200

    fixed pagination

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -40,20 +40,28 @@ export default function Pagination({ rowsPerPage, onRowsPerPageChange, page, cou
         return page === countPages - 1;
     }
 
-    function isOneBeforeLastPage() {
+    function isSecondFromTheEnd() {
         return page === countPages - 2;
     }
 
-    function isOneOfLast2Pages() {
-        return page > countPages - 3;
+    function isThirdFromTheEnd() {
+        return page === countPages - 3;
     }
 
-    function isMoreThan4Pages() {
-        return countPages > 4;
+    function isMoreThan5Pages() {
+        return countPages > 5;
     }
 
-    function isPageMoreThan2() {
-        return page > 1;
+    function isMoreThan1Page() {
+        return countPages > 1;
+    }
+
+    function isSelectedFromFirst3() {
+        return page < 3;
+    }
+
+    function isSelectedFromLast3() {
+        return page >= countPages - 3;
     }
 
     function isFirstPage() {
@@ -64,35 +72,89 @@ export default function Pagination({ rowsPerPage, onRowsPerPageChange, page, cou
         return page === 1;
     }
 
-    function isOneOfFirst2Pages() {
-        return page < 2;
+    function isThirdPage() {
+        return page === 2;
     }
 
     return (
-        <Grid container display="flex" alignItems="center" rowGap={2}>
+        <Grid container display="flex" alignItems="center" rowGap={2} mt={2}>
             <Grid item xs={12} md={8} display="flex" alignItems="center"
                   justifyContent={{ xs: 'center', md: 'flex-start' }} gap={1}>
                 <CircleClickableBox className={isFirstPage() ? 'selected' : ''}
                                     onClick={() => onPageChange(0)}>1</CircleClickableBox>
 
-                {isPageMoreThan2() && isMoreThan4Pages() && <b>...</b>}
+                {isMoreThan5Pages() && isSelectedFromFirst3() &&
+                  <>
+                    <CircleClickableBox className={isSecondPage() ? 'selected' : ''}
+                                        onClick={() => onPageChange(1)}>
+                        {2}
+                    </CircleClickableBox>
 
-                <CircleClickableBox className={isSecondPage() ? 'selected' : ''}
-                                    onClick={() => onPageChange(isOneOfFirst2Pages() ? 1 : (isOneOfLast2Pages() ? countPages - 3 : page - 1))}>
-                    {isOneOfFirst2Pages() ? 2 : (isOneOfLast2Pages() ? countPages - 2 : page)}
-                </CircleClickableBox>
+                    <CircleClickableBox className={isThirdPage() ? 'selected' : ''}
+                                        onClick={() => onPageChange(2)}>
+                        {3}
+                    </CircleClickableBox>
 
-                {isPageMoreThan2() && !isOneOfLast2Pages() && isMoreThan4Pages() &&
-                  <CircleClickableBox className="selected">{page + 1}</CircleClickableBox>}
+                    {!isFirstPage() && !isSecondPage() &&
+                        <CircleClickableBox onClick={() => onPageChange(3)}>
+                        {4}
+                    </CircleClickableBox>}
 
-                <CircleClickableBox className={isOneBeforeLastPage() ? 'selected' : ''}
-                                    onClick={() => onPageChange(isOneOfFirst2Pages() ? 2 : (isOneOfLast2Pages() ? countPages - 2 : page + 1))}>
-                    {isOneOfFirst2Pages() ? 3 : (isOneOfLast2Pages() ? countPages - 1 : page + 2)}
-                </CircleClickableBox>
+                    <b>...</b>
+                  </>}
 
-                {!isOneOfLast2Pages() && countPages > 4 && <b>...</b>}
+                {(countPages === 3 || countPages === 4 || countPages === 5) &&
+                  <CircleClickableBox className={isSecondPage() ? 'selected' : ''}
+                                      onClick={() => onPageChange(1)}>
+                      {2}
+                  </CircleClickableBox>}
 
-                {countPages > 1 &&
+                {(countPages === 4 || countPages === 5) &&
+                  <CircleClickableBox className={isThirdPage() ? 'selected' : ''}
+                                      onClick={() => onPageChange(2)}>
+                      {3}
+                  </CircleClickableBox>}
+
+                {countPages === 5 &&
+                  <CircleClickableBox className={isSecondFromTheEnd() ? 'selected' : ''}
+                                      onClick={() => onPageChange(3)}>
+                      {4}
+                  </CircleClickableBox>}
+
+                {isMoreThan5Pages() && (!isSelectedFromFirst3() && !isSelectedFromLast3()) &&
+                  <>
+                    <b>...</b>
+
+                    <CircleClickableBox onClick={() => onPageChange(page - 1)}>{page}</CircleClickableBox>
+
+                    <CircleClickableBox className="selected">{page + 1}</CircleClickableBox>
+
+                    <CircleClickableBox onClick={() => onPageChange(page + 1)}>{page + 2}</CircleClickableBox>
+
+                    <b>...</b>
+                  </>}
+
+                {isMoreThan5Pages() && isSelectedFromLast3() &&
+                  <>
+                    <b>...</b>
+
+                      {!isLastPage() && !isSecondFromTheEnd() &&
+                        <CircleClickableBox onClick={() => onPageChange(countPages - 4)}>
+                            {countPages - 3}
+                        </CircleClickableBox>}
+
+                    <CircleClickableBox className={isThirdFromTheEnd() ? 'selected' : ''}
+                                        onClick={() => onPageChange(countPages - 3)}>
+                        {countPages - 2}
+                    </CircleClickableBox>
+
+                    <CircleClickableBox className={isSecondFromTheEnd() ? 'selected' : ''}
+                                        onClick={() => onPageChange(countPages - 2)}>
+                        {countPages - 1}
+                    </CircleClickableBox>
+                  </>}
+
+                {isMoreThan1Page() &&
                   <CircleClickableBox className={isLastPage() ? 'selected' : ''}
                                       onClick={() => onPageChange(countPages - 1)}>
                       {countPages}
