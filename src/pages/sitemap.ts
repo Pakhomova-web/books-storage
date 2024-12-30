bd6f817d03520694d379e75747e04abc0a76a950
PakhomovaAnastasiia <pakhomova@sednor.net>
2024-12-30 15:45:06 +0200

    fixed sitemap

--- src/pages/sitemap.ts
+++ src/pages/sitemap.ts
@@ -1,76 +1,73 @@
 ﻿import { MetadataRoute } from 'next';
 import { BookEntity } from '@/lib/data/types';
-import { apolloClient } from '@/lib/apollo';
-import { booksQuery } from '@/lib/graphql/queries/book/queries';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const data = await apolloClient.query({
-        query: booksQuery,
-        fetchPolicy: 'no-cache'
-    });
+    // const data = await apolloClient.query({
+    //     query: booksQuery,
+    //     fetchPolicy: 'no-cache'
+    // });
+
+    const data = { books: { items: [] } };
 
     return [
-        ...[
-            {
-                url: 'https://books-storage.vercel.app',
-                lastModified: new Date(),
-                changeFrequency: 'yearly',
-                priority: 1
-            },
-            {
-                url: 'https://books-storage.vercel.app/books',
-                lastModified: new Date(),
-                changeFrequency: 'monthly',
-                priority: 0.8
-            },
-            {
-                url: 'https://books-storage.vercel.app/about-us',
-                lastModified: new Date(),
-                changeFrequency: 'weekly',
-                priority: 0.5
-            },
-            {
-                url: 'https://books-storage.vercel.app/publishing-houses',
-                lastModified: new Date(),
-                changeFrequency: 'weekly',
-                priority: 0.5
-            },
-            {
-                url: 'https://books-storage.vercel.app/sign-in',
-                lastModified: new Date(),
-                changeFrequency: 'monthly',
-                priority: 0.5
-            },
-            {
-                url: 'https://books-storage.vercel.app/basket',
-                lastModified: new Date(),
-                changeFrequency: 'weekly',
-                priority: 0.5
-            },
-            {
-                url: 'https://books-storage.vercel.app/profile/personal-info',
-                lastModified: new Date(),
-                changeFrequency: 'weekly',
-                priority: 0.5
-            },
-            {
-                url: 'https://books-storage.vercel.app/profile/likes',
-                lastModified: new Date(),
-                changeFrequency: 'weekly',
-                priority: 0.5
-            },
-            {
-                url: 'https://books-storage.vercel.app/profile/orders',
-                lastModified: new Date(),
-                changeFrequency: 'weekly',
-                priority: 0.5
-            }
-        ],
-        ...data['books'].items.map((book: BookEntity) => ({
-            url: `${process.env.FRONTEND_URL}/books/${book.id}`,
-            changeFrequency: 'monthly',
+        {
+            url: 'https://books-storage.vercel.app',
             lastModified: new Date(),
-            priority: 0.5,
+            changeFrequency: 'yearly',
+            priority: 1
+        },
+        {
+            url: 'https://books-storage.vercel.app/books',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8
+        },
+        {
+            url: 'https://books-storage.vercel.app/about-us',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5
+        },
+        {
+            url: 'https://books-storage.vercel.app/publishing-houses',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5
+        },
+        {
+            url: 'https://books-storage.vercel.app/sign-in',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.5
+        },
+        {
+            url: 'https://books-storage.vercel.app/basket',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5
+        },
+        {
+            url: 'https://books-storage.vercel.app/profile/personal-info',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5
+        },
+        {
+            url: 'https://books-storage.vercel.app/profile/likes',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5
+        },
+        {
+            url: 'https://books-storage.vercel.app/profile/orders',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5
+        },
+        ...data.books.items.map((book: BookEntity) => ({
+            url: `${process.env.FRONTEND_URL}/books/${book.id}`,
+            lastModified: new Date(),
+            priority: 0.9,
             images: book.imageIds ? [`https://drive.google.com/thumbnail?id=${book.imageIds[0]}&sz=w1000`] : []
         }))
     ];
