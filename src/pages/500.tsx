7e5283d799703f141dd150b5ed1d5c1b183d32b8
PakhomovaAnastasiia <pakhomova@sednor.net>
2025-01-25 12:31:13 +0200

    added 500 page

---
+++ src/pages/500.tsx
@@ -0,0 +1,36 @@
+import { Box, Button } from '@mui/material';
+import CustomImage from '@/components/custom-image';
+import { styleVariables } from '@/constants/styles-variables';
+import React from 'react';
+import { useRouter } from 'next/router';
+import SocialMediaBox from '@/components/social-media-box';
+import DeliveriesBox from '@/components/deliveries-box';
+
+const imageBoxStyles = {
+    width: '150px',
+    height: '150px',
+    opacity: 0.5
+};
+
+export default function ErrorPage() {
+    const router = useRouter();
+
+    return (
+        <>
+            <SocialMediaBox/>
+
+            <DeliveriesBox/>
+
+            <Box display="flex" gap={2} flexDirection="column" justifyContent="center" alignItems="center" mt={2}>
+                <Box sx={imageBoxStyles}>
+                    <CustomImage imageLink="/500_page.png"></CustomImage>
+                </Box>
+                <Box sx={styleVariables.titleFontSize}>Ой, щось пішло не так</Box>
+
+                <Box display="flex">
+                    <Button variant="outlined" onClick={() => router.push('/')}>До вибору книг</Button>
+                </Box>
+            </Box>
+        </>
+    );
+}
